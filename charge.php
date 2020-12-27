7ca7247570c851f98b0d3acaa7cc9c60ceffea56
Hyun <65524807+seonghyun26@users.noreply.github.com>
2020-12-27 18:08:00 +0900

    hackaton final result

---
+++ charge.php
@@ -0,0 +1,16 @@
+<?php
+    session_start();
+    include('conn.php');
+    $id = $_SESSION['user-id'];
+    $sql = "SELECT * FROM users WHERE id='$id'";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_array($result);
+    $money = $row['money'] + 10000;
+
+    $insert = "UPDATE users SET money='$money' where id='$id'";
+    $result_ = mysqli_query($conn, $insert);
+    echo "<script>
+    window.location.replace('./charge.html');
+    </script>";
+?>
+?>
